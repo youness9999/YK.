b572ed79fbd49ab1834e2ca39fe17211cba13ded
youness <bloodstreekarab@gmail.com>
2026-05-02 23:30:47 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
     apiKey: "YOUR_API_KEY",
     authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
+    projectId: "ykshop-6ce6f",
     storageBucket: "YOUR_STORAGE_BUCKET",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
