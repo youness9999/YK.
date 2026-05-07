4efda55125bcfab0f34d10c7b0f2d4d3db4a4f85
youness9999 <bloodstreekarab@gmail.com>
2026-05-07 19:57:07 +0100

    Fix React setup and resolve merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
-  base: './',
->>>>>>> 1863b97 (إضافة مجلد logic js فقط)
 })
+
